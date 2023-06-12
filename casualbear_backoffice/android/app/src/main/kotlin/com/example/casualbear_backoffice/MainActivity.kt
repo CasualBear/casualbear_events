8bda9a352c7f97759860317372075631afc6ad74
José Nobre <41454761+JosePedroNobre@users.noreply.github.com>
2023-06-12 17:39:01 +0100

    added backoffice and web

---
+++ casualbear_backoffice/android/app/src/main/kotlin/com/example/casualbear_backoffice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.casualbear_backoffice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ casualbear_wesbite/android/app/src/main/kotlin/com/example/casualbear_wesbite/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.casualbear_wesbite
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
